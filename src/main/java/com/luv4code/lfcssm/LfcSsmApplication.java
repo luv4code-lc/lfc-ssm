35382fad28084d7345a809b10b8f3b4672373086
luv4code-lfc <luv4code.lc@gmail.com>
2021-12-14 18:22:32 +0530

    initila setup

---
+++ src/main/java/com/luv4code/lfcssm/LfcSsmApplication.java
@@ -0,0 +1,13 @@
+package com.luv4code.lfcssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfcSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LfcSsmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luv4code/lfcssm/LfcSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv4code.lfcssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LfcSsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
